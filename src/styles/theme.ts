dd6740b64f1054af86d29bbb534b1929ca2e891a
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-08-05 00:37:57 +0200

    update styles (cleaned code)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -33,13 +33,10 @@ export interface CortexaTheme {
         primary: string;
         primaryHover: string;
         primaryTint: string;
-        accent: string;
         background: string;
         subtleBackground: string;
         textHeadings: string;
         textBody: string;
-        textPrimary: string;
-        textSecondary: string;
         borders: string;
         success: string;
         warning: string;
@@ -53,13 +50,10 @@ const light: CortexaTheme = {
         primary: '#0052FF',
         primaryHover: '#0048E0',
         primaryTint: '#E5F0FF',
-        accent: '#0052FF',
         background: '#FFFFFF',
         subtleBackground: '#F6F6F6',
         textHeadings: '#1D1D1F',
         textBody: '#545454',
-        textPrimary: '#1D1D1F',
-        textSecondary: '#545454',
         borders: '#D1D1D6',
         success: '#28A745',
         warning: '#FFC107',
@@ -73,14 +67,11 @@ const dark: CortexaTheme = {
         primary: '#0052FF',
         primaryHover: '#0048E0',
         primaryTint: '#001a52',
-        accent: '#409cff',
         background: '#1D1D1F',
-        subtleBackground: '#2c2c2e',
+        subtleBackground: '#2C2C2E',
         textHeadings: '#F5F5F7',
         textBody: '#A1A1A6',
-        textPrimary: '#F5F5F7',
-        textSecondary: '#A1A1A6',
-        borders: '#3a3a3c',
+        borders: '#3A3A3C',
         success: '#28A745',
         warning: '#FFC107',
         error: '#DC3545',
