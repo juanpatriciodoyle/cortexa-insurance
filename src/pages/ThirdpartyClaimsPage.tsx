2ecdd42de79f44e9e987ea885eaada6c421784ac
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-08-04 13:09:44 +0200

    add claims / step 3

--- src/pages/ThirdpartyClaimsPage.tsx
+++ src/pages/ThirdpartyClaimsPage.tsx
@@ -3,6 +3,7 @@ import {AnimatePresence} from 'framer-motion';
 import ClaimsLayout from '../components/claims/ClaimsLayout';
 import Step1Welcome from '../components/claims/Step1Welcome';
 import Step2IncidentDetails from '../components/claims/Step2IncidentDetails';
+import Step3UploadPhotos from '../components/claims/Step3UploadPhotos';
 
 function ThirdPartyClaimsPage() {
     const [step, setStep] = useState(1);
@@ -18,11 +19,17 @@ function ThirdPartyClaimsPage() {
         setStep(3);
     };
 
+    const handleStep3Complete = (files: File[]) => {
+        setClaimData({...claimData, files});
+        setStep(4);
+    };
+
     return (
         <ClaimsLayout>
             <AnimatePresence mode="wait">
                 {step === 1 && <Step1Welcome key="step1" onComplete={handleStep1Complete}/>}
                 {step === 2 && <Step2IncidentDetails key="step2" onComplete={handleStep2Complete}/>}
+                {step === 3 && <Step3UploadPhotos key="step3" onComplete={handleStep3Complete}/>}
             </AnimatePresence>
         </ClaimsLayout>
     );
