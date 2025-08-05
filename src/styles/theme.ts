2bf7f05bf983ea803a32f36e77d61892e38a2383
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-08-05 12:43:41 +0200

    update common colors (cleaned code)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -26,6 +26,15 @@ export const baseTheme = {
     },
 };
 
+const commonColors = {
+    primary: '#0048DF',
+    primaryHover: '#003CBC',
+    primaryTint: '#E5F0FF',
+    success: '#00B74A',
+    warning: '#FFA900',
+    error: '#F93154',
+};
+
 export interface CortexaTheme {
     font: typeof baseTheme.font;
     sizing: typeof baseTheme.sizing;
@@ -47,34 +56,24 @@ export interface CortexaTheme {
 const light: CortexaTheme = {
     ...baseTheme,
     colors: {
-        primary: '#0048DF',
-        primaryHover: '#003CBC',
-        primaryTint: '#E5F0FF',
+        ...commonColors,
         background: '#FFFFFF',
         subtleBackground: '#F6F6F6',
         textHeadings: '#1D1D1F',
         textBody: '#545454',
         borders: '#D1D1D6',
-        success: '#00B74A',
-        warning: '#FFA900',
-        error: '#F93154',
     },
 };
 
 const dark: CortexaTheme = {
     ...baseTheme,
     colors: {
-        primary: '#0048DF',
-        primaryHover: '#003CBC',
-        primaryTint: '#001A52',
+        ...commonColors,
         background: '#1D1D1F',
         subtleBackground: '#2C2C2E',
         textHeadings: '#F5F5F7',
         textBody: '#B9B9BE',
         borders: '#3A3A3C',
-        success: '#00B74A',
-        warning: '#FFA900',
-        error: '#F93154',
     },
 };
 
