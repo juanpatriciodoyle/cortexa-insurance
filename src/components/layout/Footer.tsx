718c1cecf55e9964d309b9446e590d8c331b9120
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-08-08 16:30:45 +0200

    update reduce footer height

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import {Container} from '../ui/Container';
 import Text from '../../styles/Text';
 
 const FooterWrapper = styled.footer`
-    padding: 40px 0;
+    padding: 24px 0;
     background-color: ${({theme}) => theme.colors.background};
     border-top: 1px solid ${({theme}) => theme.colors.borders};
     transition: background-color 0.3s ease, border-color 0.3s ease;
