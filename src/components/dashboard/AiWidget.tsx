e385b1cd79b58383ed389d0b0200d7388976be62
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-08-14 19:14:10 +0200

    update link to copy of Arcade

--- src/components/dashboard/AiWidget.tsx
+++ src/components/dashboard/AiWidget.tsx
@@ -45,7 +45,7 @@ function AiWidget() {
                     <WidgetTitle as="h3" $variant="h3" style={{marginBottom: 0}}>Max AI</WidgetTitle>
                     <ExploreButtonWrapper>
                         <Button $variant="tertiary"
-                                onClick={() => window.open('https://app.arcade.software/share/hUJ4p3TxGVjcy3XLoUiL?ref=share-link', '_blank')}
+                                onClick={() => window.open('https://app.arcade.software/share/gFBkcG9NzMU4x74vGqv9', '_blank')}
                         >View more insights →</Button>
                     </ExploreButtonWrapper>
                 </CoPilotHeader>
