30e3b3e5fb68a2f0d7cdc2f71f854d728eefdb2a
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-09-25 14:39:24 +0200

    add box-sizing global rule

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: content-box;
+    }
+
     body {
         margin: 0;
         font-family: ${({theme}) => theme.font.primary};
